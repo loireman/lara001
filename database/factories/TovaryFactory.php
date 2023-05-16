ee411694a1098a68d9dc0ce31a8a5dad3ba618e7
loireman <akarvasarskij@gmail.com>
2023-05-16 14:20:54 +0200

    update

--- database/factories/TovaryFactory.php
+++ database/factories/TovaryFactory.php
@@ -21,7 +21,7 @@ class TovaryFactory extends Factory
         return [
             'slug' => $this->faker->slug(),
             'name' => $this->faker->domainName(),
-            'body' => $this->faker->text(10000),
+            'body' => $this->faker->text(1000),
             'property1' => $this->faker->numberBetween(0, 1024),
             'property2' => $this->faker->numberBetween(0, 128),
             'price' => $this->faker->randomFloat(2, 0, 9999.99),
